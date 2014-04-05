2d8d8b1bf41809f4995084c5d1994fbec8cf87a8
Artur Matos <arturmatos78@gmail.com>
2014-04-05 17:46:29 +0100

    Using stackType as an enum instead of separate rows and foundations adts

--- beleagueredcastle.hs
+++ beleagueredcastle.hs
@@ -6,7 +6,8 @@ data Rank = RA | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | RJ | RQ | RK deri
 
 data Card = Card Rank Suit deriving (Show)
 
-data Stack = Foundation [Card] | Row [Card] deriving (Show)
+data StackType = Foundation | Row deriving (Show)
+data Stack = Stack [Card] StackType deriving (Show)
 
 data Board = Board [Stack] deriving (Show)
 
@@ -17,41 +18,45 @@ suit :: Card -> Suit
 suit (Card _ s) = s
 
 cards :: Stack -> [Card]
-cards (Foundation lst) = lst
-cards (Row lst) = lst
+cards (Stack cards stackType) = cards
 
-push :: Stack -> Card -> Stack
-push (Foundation cards) new = (Foundation (new:cards))
-push (Row cards) new = (Row (new:cards))
-
-pop :: Stack -> (Card, Stack)
-pop (Foundation (x:xs)) = (x, (Foundation xs))
-pop (Row (x:xs)) = (x, (Row xs))
-
-isLegalMove :: Stack -> Stack -> Bool
-isLegalMove (Row (x:_)) (Row xs) = isLegalMoveRow x xs
-isLegalMove (Row (x:_)) (Foundation xs) = isLegalMoveFoundation x xs
-isLegalMove (Foundation (x:_)) (Row xs) = isLegalMoveRow x xs
-isLegalMove _ _ = False
-
-isLegalMoveRow :: Card -> [Card] -> Bool
-isLegalMoveRow _ [] = True
-isLegalMoveRow (Card RA _) _ = False
-isLegalMoveRow (Card RK _) (x:_) = False
-isLegalMoveRow card (topRowCard:_) = succ (rank card) == rank topRowCard
-
-isLegalMoveFoundation :: Card -> [Card] -> Bool
-isLegalMoveFoundation _ ((Card RK _):_) = False
-isLegalMoveFoundation (Card x s1) ((Card y s2):_) = x == succ y && s1 == s2
-isLegalMoveFoundation _ _ = False
+stackType :: Stack -> StackType
+stackType (Stack cards stackType) = stackType
 
 
--- move :: Board -> int -> int -> Board
--- move (Board stackList) index1 index2 =
---   let fromCards = cards stackList !! index1
---       toCards = cards stackList !! index2
---       elem:rem = fromCards
---       added = elem:toCards      
---   in (Board toList(update index2 added (update index1 rem (fromList stackList))))
+-- isLegalMove :: Stack -> Stack -> Bool
+-- isLegalMove (Row (x:_)) (Row xs) = isLegalMoveRow x xs
+-- isLegalMove (Row (x:_)) (Foundation xs) = isLegalMoveFoundation x xs
+-- isLegalMove (Foundation (x:_)) (Row xs) = isLegalMoveRow x xs
+-- isLegalMove _ _ = False
+
+-- isLegalMoveRow :: Card -> [Card] -> Bool
+-- isLegalMoveRow _ [] = True
+-- isLegalMoveRow (Card RA _) _ = False
+-- isLegalMoveRow (Card RK _) (x:_) = False
+-- isLegalMoveRow card (topRowCard:_) = succ (rank card) == rank topRowCard
+
+-- isLegalMoveFoundation :: Card -> [Card] -> Bool
+-- isLegalMoveFoundation _ ((Card RK _):_) = False
+-- isLegalMoveFoundation (Card x s1) ((Card y s2):_) = x == succ y && s1 == s2
+-- isLegalMoveFoundation _ _ = False
+
+
+move :: Board -> Int -> Int -> Board
+move (Board stackList) index1 index2 =
+     let fromStack = stackList !! index1
+         toStack = stackList !! index2
+         fromType = stackType fromStack
+         toType = stackType toStack
+         (e:rem) = cards fromStack
+         toCards = cards toStack
+         resultFrom = Stack (e:toCards) fromType
+         resultTo = Stack rem toType
+     in Board (toList(update index2 resultTo (update index1 resultFrom (fromList stackList))))
+     -- in (Board (toList (fromList[])))
+
+
+generateMoves :: Board -> [Board]
+generateMoves board = [move board x y | x <-[0 .. 11] , y <- [0 .. 11], x /= y]
 
 
